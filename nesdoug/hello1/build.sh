08c33a2c427e0731c071ddbe72a2dbe7d1134665
Matheus Catarino <matheus-catarino@hotmail.com>
2024-05-08 11:46:38 -0300

    enable CI build

--- nesdoug/hello1/build.sh
+++ nesdoug/hello1/build.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 
-ROOTDIR=$HOME/zig-bootstrap/out
-ZIG_DIR=$ROOTDIR/zig-mos-x86_64-linux-musl-baseline
-LLVMMOS_SDK=$ROOTDIR/llvm-mos
-
-# Zig code
-$ZIG_DIR/zig build-obj \
+zig build-obj \
     hello1.zig \
     chr-rom.s \
     -target mos-freestanding \
     -O ReleaseSafe \
     -femit-bin=hello.zig.obj
 
-$LLVMMOS_SDK/bin/mos-nes-nrom-clang \
+mos-nes-nrom-clang \
     -O3 hello.zig.obj \
     -o hello.nes -lneslib
